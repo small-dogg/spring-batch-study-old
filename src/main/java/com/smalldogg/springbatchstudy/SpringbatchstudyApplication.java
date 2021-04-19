775bea27e219fdea4920dc1fa22ac77b0f92f92d
small-dogg <74956749+small-dogg@users.noreply.github.com>
2021-04-19 11:16:37 +0900

    chore: add Spring 2.4.5 & gradle

---
+++ src/main/java/com/smalldogg/springbatchstudy/SpringbatchstudyApplication.java
@@ -0,0 +1,13 @@
+package com.smalldogg.springbatchstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbatchstudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbatchstudyApplication.class, args);
+	}
+
+}
