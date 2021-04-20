79a18d4df870d3e35dcda3f242f390d7e1e0596f
small-dogg <74956749+small-dogg@users.noreply.github.com>
2021-04-20 22:02:42 +0900

    feature: add the tasklet tutorial

--- src/main/java/com/smalldogg/springbatchstudy/job/SimpleJobConfiguration.java
+++ src/main/java/com/smalldogg/springbatchstudy/job/SimpleJobConfiguration.java
@@ -1,5 +1,6 @@
 package com.smalldogg.springbatchstudy.job;
 
+import com.smalldogg.springbatchstudy.tasklet.TutorialTasaklet;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
@@ -15,24 +16,33 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SimpleJobConfiguration {
 
-  private final JobBuilderFactory jobBuilderFactory;
-  private final StepBuilderFactory stepBuilderFactory;
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
 
-  @Bean
-  public Job simpleJob() {
-    return jobBuilderFactory.get("simpleJob")
-        .start(simpleStep1())
-        .build();
-  }
+    @Bean
+    public Job simpleJob3() {
+        return jobBuilderFactory.get("simpleJob3")
+                .start(simpleStep1())
+                .next(simpleStep2())
+                .build();
+    }
 
-  @Bean
-  public Step simpleStep1() {
-    return stepBuilderFactory.get("simpleStep1")
-        .tasklet((contribution, chunkContext) -> {
-          log.info(">>>>>> This is Step1");
-          return RepeatStatus.FINISHED;
-        }).build();
+    @Bean
+    public Step simpleStep1() {
+        return stepBuilderFactory.get("simpleStep1")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info(">>>>>> This is Step1");
+                    return RepeatStatus.CONTINUABLE;
+                }).build();
 
-  }
+    }
+
+    @Bean
+    public Step simpleStep2() {
+        return stepBuilderFactory.get("simpleStep2")
+                .tasklet(new TutorialTasaklet())
+                .build();
+
+    }
 
 }
